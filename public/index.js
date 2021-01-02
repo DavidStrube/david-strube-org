3b2ab0639ac60cd5797bd9298cd9081c921638e2
David Strube <32229971+DavidStrube@users.noreply.github.com>
2021-01-02 16:12:58 -0600

    Delete index.js

--- public/index.js
+++
@@ -1,9 +0,0 @@
-const portraitTop = document.getElementById('portraitTop');
-
-portraitTop.addEventListener('mouseover', () => {
-    portraitTop.classList.remove('defocusEffect');
-});
-
-portraitTop.addEventListener('mouseout', () => {
-    portraitTop.classList.add('defocusEffect');
-});
